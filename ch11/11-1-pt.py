6bb77190af5420d1e1a714536b9bc79d4c80c64d
leeyeoreum02 <leeyeoreum02@gmail.com>
2023-08-21 00:36:01 +0900

    8/20 last update

--- ch11/11-1-pt.py
+++ ch11/11-1-pt.py
@@ -1,24 +1,10 @@
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from torchvision import transforms
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor
 
-
-train_data = CIFAR10(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
-test_data = CIFAR10(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
-train_loader = DataLoader(train_data, batch_size=128)
-test_loader = DataLoader(test_data, batch_size=128)
 
 n_class = 10  # 부류 수
 img_siz = (32, 32, 3)  # 영상의 크기
@@ -28,6 +14,26 @@ p2 = (img_siz[0] // patch_siz) ** 2  # 패치 개수
 d_model = 64  # 임베딩 벡터 차원
 h = 8  # 헤드 개수
 N = 6  # 인코더 블록의 개수
+
+x_train = CIFAR10(root='data', train=True, download=True).data
+transform = transforms.Compose([
+    ToTensor(),
+    transforms.Normalize(mean=x_train.mean(axis=-1), std=x_train.std(axis=-1))
+])
+train_data = CIFAR10(
+    root="data",
+    train=True,
+    download=True,
+    transform=transform,
+)
+test_data = CIFAR10(
+    root="data",
+    train=False,
+    download=True,
+    transform=transform,
+)
+train_loader = DataLoader(train_data, batch_size=128)
+test_loader = DataLoader(test_data, batch_size=128)
 
 
 class Patches(nn.Module):
@@ -40,3 +46,5 @@ class Patches(nn.Module):
         batch_size = img.shape[0]
         patches = self.extract_patches(img).permute(0, 2, 1)
         patch_dims = patches.shape[-1]
+        patches = patches.view(batch_size, -1, patch_dims)
+        return patches
